805dd40c44c4021a6b32b8a7ad4002cb4de12f87
Daniel <daniel.dimovski@jumbo.com>
2023-12-03 13:04:00 +0100

    Extract common code & format

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,10 +1,9 @@
 private class EngineSchematic(
     input: List<String>
 ) {
-
     private val matrixNumbers: Array<Array<Int?>>
+    private val validMatrix: Array<Array<Boolean>>
     val matrixSymbols: Array<Array<Char?>>
-    val validMatrix: Array<Array<Boolean>>
 
     init {
         matrixNumbers = Array(input.size) { row ->
@@ -27,45 +26,37 @@ private class EngineSchematic(
 
         validMatrix = Array(input.size) { row ->
             val length = input[row].length
-            Array(length) { column ->
+            Array(length) { _ ->
                 false
             }
         }
     }
 
-
     fun checkValidNumbers(rowIndex: Int, symbolIndex: Int) {
-        if (matrixNumbers[rowIndex][symbolIndex] !=null){
-            validMatrix[rowIndex][symbolIndex] = true
-        }
-        if (symbolIndex -1 > -1 && matrixNumbers[rowIndex][symbolIndex -1] != null){
-            validMatrix[rowIndex][symbolIndex-1] = true
-        }
-        if (symbolIndex+1 < matrixNumbers[rowIndex].size && matrixNumbers[rowIndex][symbolIndex + 1] != null) {
-            validMatrix[rowIndex][symbolIndex+1] = true
-        }
+        checkValidNumbersPerRow(rowIndex, symbolIndex)
         if (rowIndex - 1 >= 0) {
-            if (matrixNumbers[rowIndex -1 ][symbolIndex] !=null){
-                validMatrix[rowIndex - 1][symbolIndex] = true
-            }
-            if (symbolIndex -1 > -1 && matrixNumbers[rowIndex -1][symbolIndex -1] != null){
-                validMatrix[rowIndex - 1][symbolIndex-1] = true
-            }
-            if (symbolIndex+1 < matrixNumbers[rowIndex- 1].size && matrixNumbers[rowIndex-1][symbolIndex + 1] != null) {
-                validMatrix[rowIndex - 1][symbolIndex+1] = true
-            }
+            checkValidNumbersPerRow(rowIndex - 1, symbolIndex)
         }
         if (rowIndex + 1 <= matrixNumbers.size - 1) {
-            if (matrixNumbers[rowIndex +1 ][symbolIndex] !=null){
-                validMatrix[rowIndex + 1][symbolIndex] = true
-            }
-            if (symbolIndex -1 > -1 && matrixNumbers[rowIndex +1][symbolIndex -1] != null){
-                validMatrix[rowIndex + 1][symbolIndex-1] = true
-            }
-            if (symbolIndex+1 < matrixNumbers[rowIndex+1].size && matrixNumbers[rowIndex+1][symbolIndex + 1] != null) {
-                validMatrix[rowIndex + 1][symbolIndex+1] = true
-            }
+            checkValidNumbersPerRow(rowIndex + 1, symbolIndex)
         }
+    }
+
+    private fun checkValidNumbersPerRow(rowIndex: Int, symbolIndex: Int) {
+        if (matrixNumbers[rowIndex][symbolIndex] != null) {
+            validMatrix[rowIndex][symbolIndex] = true
+        }
+        if (symbolIndex - 1 > -1 && matrixNumbers[rowIndex][symbolIndex - 1] != null) {
+            validMatrix[rowIndex][symbolIndex - 1] = true
+        }
+        if (symbolIndex + 1 < matrixNumbers[rowIndex].size && matrixNumbers[rowIndex][symbolIndex + 1] != null) {
+            validMatrix[rowIndex][symbolIndex + 1] = true
+        }
+    }
+
+    fun calculateSumOfEngineNumbers(): Int {
+        val validNumbers = getValidNumbers { rowIndex, columnIndex -> validMatrix[rowIndex][columnIndex] }
+        return validNumbers.sum()
     }
 
     fun calculateSumOfEngineGears(): Int {
@@ -75,72 +66,54 @@ private class EngineSchematic(
                 if (symbol == '*') {
                     gearRatios.add(calculateGearRatio(rowIndex, columnIndex))
                 }
-
             }
-
         }
         return gearRatios.sum()
     }
 
-
-    private fun getValidNumbers(rowIndex: Int, symbolIndex: Int) :List<Int> {
-        val validNumbers = mutableListOf<Int>()
-        var integer = ""
-        var valid = false
-        matrixNumbers[rowIndex].forEachIndexed() { columnIndex, char ->
-            if (char != null) {
-                integer += char
-                if (symbolIndex == columnIndex || symbolIndex == columnIndex -1 || symbolIndex == columnIndex +1)
-                    valid = true
-            } else {
-                if (valid) validNumbers.add(integer.toInt())
-                valid = false
-                integer = ""
-            }
-        }
-        if (valid) validNumbers.add(integer.toInt())
-        validNumbers.forEach {
-            kotlin.io.println()
-        }
-        return validNumbers
-    }
     private fun calculateGearRatio(rowIndex: Int, symbolIndex: Int): Int {
         val validNumbers = mutableListOf<Int>()
-        validNumbers.addAll(getValidNumbers(rowIndex,symbolIndex))
-        if (rowIndex -1 >= 0) validNumbers.addAll(getValidNumbers(rowIndex - 1, symbolIndex))
-        if (rowIndex + 1 < matrixNumbers[rowIndex].size) validNumbers.addAll(getValidNumbers(rowIndex + 1, symbolIndex))
-
+        validNumbers.addAll(getValidNumbersPerRow(rowIndex, symbolIndex))
+        if (rowIndex - 1 >= 0) validNumbers.addAll(getValidNumbersPerRow(rowIndex - 1, symbolIndex))
+        if (rowIndex + 1 < matrixNumbers[rowIndex].size) validNumbers.addAll(
+            getValidNumbersPerRow(
+                rowIndex + 1,
+                symbolIndex
+            )
+        )
         return if (validNumbers.size == 2) validNumbers[0] * validNumbers[1]
         else 0
     }
 
-    fun calculateSumOfEngineNumbers(): Int {
+    private fun getValidNumbersPerRow(rowIndex: Int, symbolIndex: Int): List<Int> {
+        return getValidNumbers(rowIndex) { _, columnIndex ->
+            symbolIndex == columnIndex || symbolIndex == columnIndex - 1 || symbolIndex == columnIndex + 1
+        }
+    }
+    private fun getValidNumbers(rowIndexToCheck: Int? = null, validityCheck: (Int, Int) -> Boolean): List<Int> {
         val validNumbers = mutableListOf<Int>()
         matrixNumbers.forEachIndexed { rowIndex, numbersRow ->
-            var integer = ""
-            var isValid = false
-            numbersRow.forEachIndexed { columnIndex, number ->
-                if (number != null) {
-                    print(number)
-                    integer += number
-                    if (validMatrix[rowIndex][columnIndex]) isValid = true
-                } else {
-                    print(".")
-                    if (isValid) {
-                        validNumbers += integer.toInt()
+            if (rowIndexToCheck == null || rowIndexToCheck == rowIndex) {
+                var integer = ""
+                var isValid = false
+                numbersRow.forEachIndexed { columnIndex, number ->
+                    if (number != null) {
+                        integer += number
+                        if (validityCheck(rowIndex, columnIndex)) isValid = true
+                    } else {
+                        if (isValid) {
+                            validNumbers += integer.toInt()
+                        }
+                        isValid = false
+                        integer = ""
                     }
-                    isValid = false
-                    integer = ""
                 }
+                if (isValid) validNumbers.add(integer.toInt())
             }
-            if (isValid) validNumbers.add(integer.toInt())
-            println()
         }
-        validNumbers.forEach {
-            kotlin.io.println(it)
-        }
-        return validNumbers.sum()
+        return validNumbers
     }
+
 }
 
 fun main() {
@@ -164,14 +137,11 @@ fun main() {
         val engineSchematic = EngineSchematic(input)
         engineSchematic.matrixSymbols.forEachIndexed { rowIndex, row ->
             row.forEachIndexed { symbolIndex, symbol ->
-                print(symbol.toString() + ".")
                 symbol?.let {
                     if (it == '*') engineSchematic.checkValidNumbers(rowIndex, symbolIndex)
                 }
             }
-            println()
         }
-
         return engineSchematic.calculateSumOfEngineGears()
     }
 
