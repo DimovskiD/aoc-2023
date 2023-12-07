93e0784ee7a0b6c5f9e84d4dc98a406b20a0d172
Daniel <daniel.dimovski@jumbo.com>
2023-12-07 13:49:36 +0100

    Day 7 part two solution

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,78 +1,130 @@
+import CamelPokerRules.Companion.cards
+
 val JOKER_CARD = CamelCard('J')
 
-val cards = listOf(
-    CamelCard('A'),
-    CamelCard('K'),
-    CamelCard('Q'),
-    CamelCard('T'),
-    CamelCard('9'),
-    CamelCard('8'),
-    CamelCard('7'),
-    CamelCard('6'),
-    CamelCard('5'),
-    CamelCard('4'),
-    CamelCard('3'),
-    CamelCard('2'),
-    CamelCard('J'),
-)
-
-val handOfCardsTypes = listOf(
-    HandOfCardsType(
-        type = "5oK",
-        improver = { card -> if (card.cards.first() == JOKER_CARD) CamelCard('A') else null }
-    ) { it.mappedCards.size == 1 },
-    HandOfCardsType(
-        type = "4oK",
-        improver = { handOfCards ->
-            val improver = handOfCards.mappedCards.find { it.second == 4 }?.first
-            if (improver == JOKER_CARD) handOfCards.mappedCards.find { it.first != JOKER_CARD }?.first else improver
-        }) { handOfCards ->
-        handOfCards.cards.distinct().any { _ ->
-            handOfCards.mappedCards.any { it.second == 4 }
-        }
-    },
-    HandOfCardsType(
-        type = "FH",
-        improver = { handOfCards ->
-            val improver = handOfCards.mappedCards.find { it.second == 3 }?.first
-            if (improver == JOKER_CARD) handOfCards.mappedCards.find { it.second == 2 }?.first else improver
-        }
-    ) { handOfCards ->
-        handOfCards.mappedCards.size == 2 && handOfCards.mappedCards.any { it.second == 3 } && handOfCards.mappedCards.any { it.second == 2 }
-    },
-    HandOfCardsType(
-        type = "3oK",
-        improver = { handOfCards ->
-            val improver = handOfCards.mappedCards.find { it.second == 3 }?.first
-            if (improver == JOKER_CARD) handOfCards.mappedCards.map { it.first }.maxOf { it } else improver
-        }
-    ) { handOfCards ->
-        handOfCards.mappedCards.size == 3 && handOfCards.mappedCards.any { it.second == 3 } && handOfCards.mappedCards.none { it.second == 2 }
-    },
-    HandOfCardsType(
-        type = "2P",
-        improver = { handOfCards ->
-            val firstPair = handOfCards.mappedCards.first { it.second == 2 }.first
-            val secondPair = handOfCards.mappedCards.last { it.second == 2 }.first
-            if (secondPair == JOKER_CARD) firstPair else if (firstPair == JOKER_CARD) secondPair
-            else if (firstPair < secondPair) firstPair else secondPair
-        }
-    ) { handOfCards ->
-        handOfCards.mappedCards.size == 3 && handOfCards.mappedCards.count { it.second == 2 } == 2
-    },
-    HandOfCardsType("1P", { handOfCards ->
-        val improver = handOfCards.mappedCards.find { it.second == 2 }?.first
-        if (improver == JOKER_CARD) handOfCards.mappedCards.map { it.first }.maxOf { it } else improver
-    }) { handOfCards ->
-        handOfCards.mappedCards.size == 4 && handOfCards.mappedCards.count { it.second == 2 } == 1
-    },
-    HandOfCardsType("HC", { handOfCards -> handOfCards.mappedCards.map { it.first }.maxOf { it } }) { handOfCards ->
-        handOfCards.mappedCards.size == 5
+data class CamelPokerRules(val isJokerMode: Boolean) {
+    companion object {
+        val cards = mutableListOf(
+            CamelCard('A'),
+            CamelCard('K'),
+            CamelCard('Q'),
+            CamelCard('T'),
+            CamelCard('9'),
+            CamelCard('8'),
+            CamelCard('7'),
+            CamelCard('6'),
+            CamelCard('5'),
+            CamelCard('4'),
+            CamelCard('3'),
+            CamelCard('2'),
+        )
     }
-)
 
+    init {
+        if (isJokerMode) cards.add(JOKER_CARD)
+        else cards.add(3, JOKER_CARD)
+    }
+
+    private val handOfCardsTypes = listOf(
+        HandOfCardsType(
+            type = "5oK",
+            improverCard = { card -> if (card.cards.first() == JOKER_CARD) cards[0] else null }
+        ) { it.mappedCards.size == 1 },
+        HandOfCardsType(
+            type = "4oK",
+            improverCard = { handOfCards ->
+                val improver = handOfCards.mappedCards.find { it.second == 4 }?.first
+                if (improver == JOKER_CARD) {
+                    handOfCards.mappedCards.find { it.first != JOKER_CARD }?.first
+                } else improver
+            }
+        ) { handOfCards ->
+            handOfCards.cards.distinct().any { _ ->
+                handOfCards.mappedCards.any { it.second == 4 }
+            }
+        },
+        HandOfCardsType(
+            type = "FH",
+            improverCard = { handOfCards ->
+                val improver = handOfCards.mappedCards.find { it.second == 3 }?.first
+                if (improver == JOKER_CARD) {
+                    handOfCards.mappedCards.find { it.second == 2 }?.first
+                } else improver
+            }
+        ) { handOfCards ->
+            handOfCards.mappedCards.size == 2 && handOfCards.mappedCards.any { it.second == 3 } && handOfCards.mappedCards.any { it.second == 2 }
+        },
+        HandOfCardsType(
+            type = "3oK",
+            improverCard = { handOfCards ->
+                val improver = handOfCards.mappedCards.find { it.second == 3 }?.first
+                if (improver == JOKER_CARD) {
+                    handOfCards.mappedCards.map { it.first }.maxOf { it }
+                } else improver
+            }
+        ) { handOfCards ->
+            handOfCards.mappedCards.size == 3 && handOfCards.mappedCards.any { it.second == 3 } && handOfCards.mappedCards.none { it.second == 2 }
+        },
+        HandOfCardsType(
+            type = "2P",
+            improverCard = { handOfCards ->
+                val firstPair = handOfCards.mappedCards.first { it.second == 2 }.first
+                val secondPair = handOfCards.mappedCards.last { it.second == 2 }.first
+                if (secondPair == JOKER_CARD || firstPair != JOKER_CARD && firstPair < secondPair) {
+                    firstPair
+                } else secondPair
+            }
+        ) { handOfCards ->
+            handOfCards.mappedCards.size == 3 && handOfCards.mappedCards.count { it.second == 2 } == 2
+        },
+        HandOfCardsType(
+            type = "1P",
+            improverCard = { handOfCards ->
+                val improver = handOfCards.mappedCards.find { it.second == 2 }?.first
+                if (improver == JOKER_CARD) {
+                    handOfCards.mappedCards.map { it.first }.maxOf { it }
+                } else improver
+            }
+        ) { handOfCards ->
+            handOfCards.mappedCards.size == 4 && handOfCards.mappedCards.count { it.second == 2 } == 1
+        },
+        HandOfCardsType(
+            type = "HC",
+            improverCard = { handOfCards -> handOfCards.mappedCards.map { it.first }.maxOf { it } }
+        ) { handOfCards ->
+            handOfCards.mappedCards.size == 5
+        }
+    )
+
+    private fun getTypeOfHand(handOfCards: HandOfCards) = handOfCardsTypes.find { it.match(handOfCards) }
+
+    fun compareHands(thisHand: HandOfCards, otherHand: HandOfCards): Int {
+        val indexOfThisImprovedHand = handOfCardsTypes.indexOf(getTypeOfHand(bestHand(thisHand)))
+        val indexOfOtherImprovedHand = handOfCardsTypes.indexOf(getTypeOfHand(bestHand(otherHand)))
+
+        if (indexOfOtherImprovedHand == indexOfThisImprovedHand) {
+            thisHand.cards.zip(otherHand.cards) { thisCard, otherCard ->
+                if (thisCard != otherCard) return@compareHands thisCard.compareTo(otherCard)
+            }
+        }
+        return indexOfOtherImprovedHand
+            .compareTo(indexOfThisImprovedHand)
+    }
+
+    private fun bestHand(handOfCards: HandOfCards): HandOfCards {
+        if (!handOfCards.cards.contains(JOKER_CARD) || !isJokerMode) return handOfCards
+        val improverCard = this.getTypeOfHand(handOfCards)?.improverCard?.invoke(handOfCards)
+        return if (improverCard != null) {
+            val newCards = handOfCards.cards.toMutableList()
+            newCards.replaceAll { if (it == JOKER_CARD) improverCard else it }
+            HandOfCards(newCards, handOfCards.bid, handOfCards.rules)
+        } else handOfCards
+    }
+
+}
 
 data class CamelCard(val label: Char) : Comparable<CamelCard> {
+
     override fun compareTo(other: CamelCard): Int {
         return cards.indexOf(other).compareTo(cards.indexOf(this))
     }
@@ -82,29 +134,11 @@ data class CamelCard(val label: Char) : Comparable<CamelCard> {
     }
 }
 
-data class HandOfCards(val cards: List<CamelCard>, val bid: Int) : Comparable<HandOfCards> {
+data class HandOfCards(val cards: List<CamelCard>, val bid: Int, val rules: CamelPokerRules) : Comparable<HandOfCards> {
     val mappedCards = cards.distinct().map { card -> card to cards.count { it.label == card.label } }
-    private fun getTypeOfHand() = handOfCardsTypes.find { it.match(this) }
-    override fun compareTo(other: HandOfCards): Int {
-        val indexOfThis = handOfCardsTypes.indexOf(this.bestHand().getTypeOfHand())
-        val indexOfOther = handOfCardsTypes.indexOf(other.bestHand().getTypeOfHand())
-        if (indexOfOther == indexOfThis) {
-            cards.zip(other.cards) { thisCard, otherCard ->
-                if (thisCard != otherCard) return@compareTo thisCard.compareTo(otherCard)
-            }
-        }
-        return handOfCardsTypes.indexOf(other.bestHand().getTypeOfHand())
-            .compareTo(handOfCardsTypes.indexOf(this.bestHand().getTypeOfHand()))
-    }
 
-    private fun bestHand(): HandOfCards {
-        if (!this.cards.contains(JOKER_CARD)) return this
-        val improverCard = this.getTypeOfHand()?.improver?.invoke(this)
-        return if (improverCard != null) {
-            val newCards = cards.toMutableList()
-            newCards.replaceAll { if (it.label == 'J') improverCard else it }
-            HandOfCards(newCards, bid)
-        } else this
+    override fun compareTo(other: HandOfCards): Int {
+        return rules.compareHands(this, other)
     }
 
     override fun toString(): String {
@@ -114,7 +148,7 @@ data class HandOfCards(val cards: List<CamelCard>, val bid: Int) : Comparable<Ha
 
 data class HandOfCardsType(
     val type: String,
-    val improver: (HandOfCards) -> CamelCard?,
+    val improverCard: (HandOfCards) -> CamelCard?,
     private val condition: (HandOfCards) -> Boolean
 ) {
     fun match(handOfCards: HandOfCards) = condition(handOfCards)
@@ -122,22 +156,27 @@ data class HandOfCardsType(
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        val handsOfCards = input.map {
-            val split = it.split(" ")
+    fun calculateStrengthOfCards(rules: CamelPokerRules, input: List<String>): Int {
+        val handsOfCards = input.map { handOfCard ->
+            val split = handOfCard.split(" ")
             val cards = split[0].map { char -> CamelCard(char) }
-            HandOfCards(cards, split[1].toInt())
+            HandOfCards(cards, split[1].toInt(), rules)
         }
         val sortedCards = handsOfCards.sorted()
-        println(sortedCards)
         return sortedCards.sumOf { it.bid * (sortedCards.indexOf(it) + 1) }
     }
 
+    fun part1(input: List<String>): Int {
+        val rules = CamelPokerRules(false)
+        return calculateStrengthOfCards(rules, input)
+    }
+
     fun part2(input: List<String>): Int {
-        return 0
+        val rules = CamelPokerRules(true)
+        return calculateStrengthOfCards(rules, input)
     }
 
     val testInput = readInput("day07")
-    println(part1(testInput))
+    println(part2(testInput))
 
 }
