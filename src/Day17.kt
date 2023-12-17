420fcdb8648be877ee2daf6b34e2f4b780044ce8
Daniel <daniel.dimovski@jumbo.com>
2023-12-17 21:14:03 +0100

    Day 17 part 2 solution

--- src/Day17.kt
+++ src/Day17.kt
@@ -2,7 +2,6 @@ import helpers.Coordinates
 import helpers.MovementDirection
 import java.util.PriorityQueue
 
-
 data class StateToCost(val state: HeatMap.State, val cost: Int) : Comparable<StateToCost> {
     override fun compareTo(other: StateToCost): Int {
         return cost compareTo other.cost
@@ -17,7 +16,7 @@ class HeatMap(input: List<String>) {
         }
     }
 
-    fun findMinHeatCost(initialStates: List<State>): Int {
+    fun findMinHeatCost(initialStates: List<State>, minBlocks: Int, maxBlocks: Int): Int {
         val dijkstraTravelCost = mutableMapOf<State, Int>().withDefault { Int.MAX_VALUE }
         val toVisit = PriorityQueue<StateToCost>()
 
@@ -26,14 +25,14 @@ class HeatMap(input: List<String>) {
             toVisit.add(StateToCost(state, 0))
         }
 
-        val end = Coordinates(matrix[0].size - 1, matrix.size -1)
+        val end = Coordinates(matrix[0].size - 1, matrix.size - 1)
         while (toVisit.isNotEmpty()) {
             val current = toVisit.poll()
-            if (current.state.currentLocation == end) {
+            if (current.state.currentLocation == end && current.state.blocks >= minBlocks) {
                 return current.cost
             }
 
-            current.state.next()
+            current.state.next(minBlocks, maxBlocks)
                 .filter { it.currentLocation.y in matrix.indices && it.currentLocation.x in matrix[0].indices }
                 .forEach { next ->
                     val newCost = current.cost + matrix[next.currentLocation.y][next.currentLocation.x]
@@ -46,15 +45,9 @@ class HeatMap(input: List<String>) {
         return 0
     }
 
-    data class State(val currentLocation: Coordinates, val directionCoordinates: Coordinates, val blocks: Int) {
-        fun next() = buildList {
-            val turnLeft = Coordinates(x = directionCoordinates.y, y = directionCoordinates.x)
-            val turnRight = Coordinates(x = -directionCoordinates.y,y = -directionCoordinates.x)
-
-            add(copy(currentLocation = currentLocation.add(turnLeft), directionCoordinates = turnLeft, blocks = 1))
-            add(copy(currentLocation = currentLocation.add(turnRight), directionCoordinates = turnRight, blocks = 1))
-
-            if (blocks < 3) {
+    data class State(val currentLocation: Coordinates, val directionCoordinates: Coordinates, val blocks: Int = 0) {
+        fun next(minBlocks: Int, maxBlocks: Int) = buildList {
+            if (blocks < minBlocks) {
                 add(
                     copy(
                         currentLocation = currentLocation.add(directionCoordinates),
@@ -62,6 +55,34 @@ class HeatMap(input: List<String>) {
                         blocks = blocks + 1
                     )
                 )
+            } else {
+                val turnLeft = Coordinates(x = directionCoordinates.y, y = directionCoordinates.x)
+                val turnRight = Coordinates(x = -directionCoordinates.y, y = -directionCoordinates.x)
+
+                add(
+                    copy(
+                        currentLocation = currentLocation.add(turnLeft),
+                        directionCoordinates = turnLeft,
+                        blocks = 1
+                    )
+                )
+                add(
+                    copy(
+                        currentLocation = currentLocation.add(turnRight),
+                        directionCoordinates = turnRight,
+                        blocks = 1
+                    )
+                )
+
+                if (blocks < maxBlocks) {
+                    add(
+                        copy(
+                            currentLocation = currentLocation.add(directionCoordinates),
+                            directionCoordinates = directionCoordinates,
+                            blocks = blocks + 1
+                        )
+                    )
+                }
             }
         }
     }
@@ -76,16 +97,31 @@ fun main() {
                 HeatMap.State(
                     Coordinates(0, 0),
                     MovementDirection.RIGHT.coordinates,
-                    0
+                ),
+                HeatMap.State(
+                    Coordinates(0, 0),
+                    MovementDirection.DOWN.coordinates,
                 )
-            )
+            ), 0, 3
         )
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val heatMap = HeatMap(input)
+        return heatMap.findMinHeatCost(
+            listOf(
+                HeatMap.State(
+                    Coordinates(0, 0),
+                    MovementDirection.RIGHT.coordinates,
+                ),
+                HeatMap.State(
+                    Coordinates(0, 0),
+                    MovementDirection.DOWN.coordinates,
+                )
+            ), 4, 10
+        )
     }
 
     val testInput = readInput("day17")
-    println(part1(testInput))
+    println(part2(testInput))
 }
